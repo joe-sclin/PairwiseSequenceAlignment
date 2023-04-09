04b775fa4970521c5d374dfb88ae29ef2ce67e78
Joe Lin <102379756+joe-sclin@users.noreply.github.com>
2023-04-09 11:25:00 +0200

    New feature protein sequence alignment was added 2 sets of real example dataset sequences from GenBank and their results were added

--- README.md
+++ README.md
@@ -1,13 +1,27 @@
 # PairwiseSequenceAlignment
 * Implementation of pairwise Sequence Alignment using dynamic programming approach
-* User guide: program argument sequence.fasta (or other txt files) containing 2 or more DNA / RNA sequences with no degenerated code (Only A / C / G / T / U)
-* DNA / RNA sequences are case insensitive
+
+## User guide
+* User guide: program argument sequence.fasta (or other txt files) containing 2 or more DNA / RNA / amino acid sequences with no degenerated code
+* DNA / RNA / amino acid sequences are case insensitive
 * Result is exported to a txt file and on screen
-* 4 variants were included
-    * Global alignment (Needleman-Wunsch algorithm)
-    * Local alignment (Smith-Waterman algorithm)
-    * Semi-global alignment (Modified Needleman-Wunsch)
-    * Overlap alignment (Modified Smith-Waterman)
-    * Gap penalty at the end of sequence were removed in semi-global and overlap alignment
+
+## Variants of alignment algorithms
+* Global alignment (Needleman-Wunsch algorithm)
+* Local alignment (Smith-Waterman algorithm)
+* Semi-global alignment (Modified Needleman-Wunsch)
+* Overlap alignment (Modified Smith-Waterman)
+* Gap penalty at the end of sequence were removed in semi-global and overlap alignment
+
+## Approaches used
 * Traceback function used dynamic programming
   * Not required to store the traceback path --> only store the aligned part started from the end of sequences
+* Function pointers were used to select different score function for DNA/RNA or protein
+  * Users are allowed to alter Match, Mismatch and Gap score for DNA/RNA seuqneces
+  * BLOSUM62 scoring matrix was used for amino acid sequences
+    * Runtime of amino acid sequences would be a bit longer (Using 2D vector matrix and mapping to obtain score for each comparison)
+
+## Example dataset and results
+* Genomic sequence of human and house cat Cyclin B1 genes for DNA alignment
+* Hepatitis B virus polymerase protein sequence between wild-type(YMDD motif) and partial sequence from a mutant causing lamivudine-resistance in clinical outcome
+* GenBank accession number were included in header of each sequence in example dataset fasta file (Reference of each sequence could obtain from GenBank)
--- main.cpp
+++ main.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <algorithm>
 #include <vector>
+#include <map>
 
 using namespace std;
 
 signed int Match = 1;       // Initialise scoring variable using standard value
-signed int Mismatch = -1;
+signed int Mismatch = -1;   // Match and mismatch only applied to DNA / RNA alignment
 signed int Gap = -2;
+vector<vector<int>> aa_matrix;  // Only applied to protein alignment
+map<string , int> aa_mapping;
 const int HOME = 0;        // Global const variable for direction in scoring matrix (0 = binary 00)
 const int DIAGONAL = 1;    // (binary 01)
 const int HORIZONTAL = 2;  // (binary 10)
 const int VERTICAL = 4;    // (binary 100)
 
-signed int score(char a, char b = '\0') {      // Scoring function for DNA / RNA base comparison in alignment algorithms
+
+signed int base_score(char a, char b = '\0') {  // Score function for DNA / RNA
+    return (a == '\0' && b == '\0') ? Gap : (a == b) ? Match : Mismatch;
+}
+signed int aa_score(char a, char b) {       // Score function for protein (Refer to mapping on matrix --> Slower than static value in DNA / RNA)
     if (a == '\0' && b == '\0') {
-        return Gap;
+        return aa_matrix[aa_mapping.at("*")][aa_mapping.at("*")];
     } else {
-        if (a == b) {
-            return Match;
-        } else {
-            return Mismatch;
-        }
+        string str_a(1, a);
+        string str_b(1, b);
+        return aa_matrix[aa_mapping.at(str_a)][aa_mapping.at(str_b)];
     }
+}
+
+void BLOSUM_matrix() {   // Modify BLOSUM_matrix for Match, Mismatch and Gap score
+    ifstream BLOSUM_file("BLOSUM62.csv");
+    vector<vector<int>> BLOSUM62;       // Vector can only store matrix value
+    map<string , int> aa_map;
+    string currentline;
+    while (getline(BLOSUM_file, currentline)) {
+        stringstream ss(currentline);
+        // Skip the first row
+        if (currentline[0] == ',') {    // First row (List of amino acid)
+            int cols = -1; // First row and column are skipped (space is mapped to -1)
+            string aminoacid;
+            while (getline(ss, aminoacid, ',')) {
+                aa_map[aminoacid] = cols;
+                cols++;
+            }
+            continue;
+        }
+        vector<int> currentrow;
+        string value;
+        getline(ss, value, ',');
+        while (getline(ss, value, ',')) {
+            currentrow.push_back(stoi(value));
+        }
+        BLOSUM62.push_back(currentrow);
+    }
+    BLOSUM_file.close();
+    aa_matrix = BLOSUM62;
+    aa_mapping = aa_map;
 }
 
 // Traceback function to generate aligned sequences by dynamic programming approach (Not storing the traceback path, only storing aligned part of sequences)
@@ -52,7 +88,7 @@ pair<string, string> traceback(unsigned int i, unsigned int j, vector<vector<int
 }
 
 // Section: implementation of 4 alignment variants
-pair<string, string> global(string& x, string& y) {     // Needleman-Wunsch algorithm
+pair<string, string> global(string& x, string& y, int(*score)(char, char)) {     // Needleman-Wunsch algorithm
     unsigned int m = x.length();
     unsigned int n = y.length();
     vector<vector<int>> Sc(m+1, vector<int>(n+1)); // scores matrix
@@ -70,7 +106,7 @@ pair<string, string> global(string& x, string& y) {     // Needleman-Wunsch algo
         char xi = x[i-1];
         for (int j = 1; j <= n; j++) {
             char yj = y[j-1];
-            int d = Sc[i-1][j-1] + score(xi, yj);
+            int d = Sc[i-1][j-1] + (*score)(xi, yj);
             int h = Sc[i][j-1] + Gap;
             int v = Sc[i-1][j] + Gap;
             int best_dir = max({d, h, v});      // Select the direction with the best score (the highest value)
@@ -84,7 +120,7 @@ pair<string, string> global(string& x, string& y) {     // Needleman-Wunsch algo
     return alignedseq;
 }
 
-pair<string, string> local(string& x, string& y) {     // Smith-Waterman algorithm
+pair<string, string> local(string& x, string& y, int(*score)(char, char)) {     // Smith-Waterman algorithm
     unsigned int m = x.length();
     unsigned int n = y.length();
     vector<vector<int>> Sc(m+1, vector<int>(n+1));
@@ -102,7 +138,7 @@ pair<string, string> local(string& x, string& y) {     // Smith-Waterman algorit
         char xi = x[i-1];
         for (int j = 1; j <= n; j++) {
             char yj = y[j-1];
-            int d = Sc[i-1][j-1] + score(xi, yj);
+            int d = Sc[i-1][j-1] + (*score)(xi, yj);
             int h = Sc[i][j-1] + Gap;
             int v = Sc[i-1][j] + Gap;
             int best_dir = max({0, d, h, v});      // 0 is also included (Not in global alignment)
@@ -123,7 +159,7 @@ pair<string, string> local(string& x, string& y) {     // Smith-Waterman algorit
     return alignedseq;
 }
 
-pair<string, string> semiglobal(string& x, string& y) {     // Modified global alignment (No gap penalty at the end of either sequences)
+pair<string, string> semiglobal(string& x, string& y, int(*score)(char, char)) {     // Modified global alignment (No gap penalty at the end of either sequences)
     unsigned int m = x.length();
     unsigned int n = y.length();
     vector<vector<int>> Sc(m+1, vector<int>(n+1));
@@ -143,7 +179,7 @@ pair<string, string> semiglobal(string& x, string& y) {     // Modified global a
         char xi = x[i-1];
         for (int j = 1; j <= n; j++) {
             char yj = y[j-1];
-            int d = Sc[i-1][j-1] + score(xi, yj);
+            int d = Sc[i-1][j-1] + (*score)(xi, yj);
             int h = Sc[i][j-1] + Gap;
             int v = Sc[i-1][j] + Gap;
             int best_dir = max({ d, h, v});
@@ -162,7 +198,7 @@ pair<string, string> semiglobal(string& x, string& y) {     // Modified global a
     return alignedseq;
 }
 
-pair<string, string> overlap(string& x, string& y) {     // Modified local alignment (No gap penalty at the end of either sequences)
+pair<string, string> overlap(string& x, string& y, int(*score)(char, char)) {     // Modified local alignment (No gap penalty at the end of either sequences)
     unsigned int m = x.length();
     unsigned int n = y.length();
     vector<vector<int>> Sc(m + 1, vector<int>(n + 1));
@@ -179,7 +215,7 @@ pair<string, string> overlap(string& x, string& y) {     // Modified local align
         char xi = x[i - 1];
         for (int j = 1; j <= n; j++) {
             char yj = y[j - 1];
-            int d = Sc[i - 1][j - 1] + score(xi, yj);
+            int d = Sc[i - 1][j - 1] + (*score)(xi, yj);
             int h = Sc[i][j - 1] + Gap;
             int v = Sc[i - 1][j] + Gap;
             if (i == 0 || j == 0){
@@ -206,14 +242,14 @@ pair<string, string> overlap(string& x, string& y) {     // Modified local align
     return alignedseq;
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]) {
     if (argc != 2) {    // Check number of input argument
         cerr << "Error: Format of input: " << argv[0] << " sequence.fasta" << endl;
         exit(1);
     } else {
         // Section: fasta file checking
         ifstream fasta(argv[1]);
+
         if (!fasta.is_open()) {     // Check whether fasta files can open
             cerr << "Error: could not open fasta file" << endl;
             exit(1);
@@ -246,34 +282,79 @@ int main(int argc, char* argv[])
         transform(seq1.begin(), seq1.end(), seq1.begin(), ::toupper);
         transform(seq2.begin(), seq2.end(), seq2.begin(), ::toupper);
 
-        // check that all characters are A/T/C/G/U (Alignment of DNA / RNA are allowed)
-        for (char c: seq1) {
-            if (c != 'A' && c != 'T' && c != 'C' && c != 'G' && c != 'U') {
-                cerr << "Error: Invalid character in Sequence 1, only A / C / G / T / U are allowed." << endl;
-                exit(1);
-            }
-        }
-        for (char c: seq2) {
-            if (c != 'A' && c != 'T' && c != 'C' && c != 'G' && c != 'U') {
-                cerr << "Error: Invalid character in Sequence 2, only A / C / G / T / U are allowed." << endl;
-                exit(1);
-            }
-        }
-        // Section: change scores setting
-        char change;
-        cout << "Do you want to change default score setting (Y or N)? (Match: " << Match << ", Mismatch: " << Mismatch
-             << ", Gap: " << Gap << endl;
-        cin >> change;
-        if (change != 'Y' && change != 'N') {
-            cerr << "Error: Only Y or N is allowed." << endl;
+        // Section: input sequences checking
+        string choice;
+        cout << "Which type of alignment do you want (DNA / RNA / protein)" << endl;
+        cin >> choice;
+        if (choice != "DNA" && choice != "RNA" && choice != "protein") {
+            cerr << "Error: Only DNA / RNA / protein is allowed." << endl;
             exit(1);
-        } else if (change == 'Y') {
-            cout << "Please enter the new Match, Mismatch, Gap score:" << endl;
-            cin >> Match >> Mismatch >> Gap;
-            if (cin.fail()){
-                cerr << "Error: Only integer scores are allowed." << endl;
-                exit(1);
+        } else if (choice == "DNA") {   // check that all characters are A/T/C/G
+            for (char c: seq1) {
+                if (c != 'A' && c != 'T' && c != 'C' && c != 'G') {
+                    cerr << "Error: Invalid character in Sequence 1, only A / C / G / T are allowed." << endl;
+                    exit(1);
+                }
             }
+            for (char c: seq2) {
+                if (c != 'A' && c != 'T' && c != 'C' && c != 'G') {
+                    cerr << "Error: Invalid character in Sequence 2, only A / C / G / T are allowed." << endl;
+                    exit(1);
+                }
+            }
+        } else if (choice == "RNA") {   // check that all characters are A/U/C/G
+            for (char c: seq1) {
+                if (c != 'A' && c != 'U' && c != 'C' && c != 'G') {
+                    cerr << "Error: Invalid character in Sequence 1, only A / C / G / U are allowed." << endl;
+                    exit(1);
+                }
+            }
+            for (char c: seq2) {
+                if (c != 'A' && c != 'U' && c != 'C' && c != 'G') {
+                    cerr << "Error: Invalid character in Sequence 2, only A / C / G / U are allowed." << endl;
+                    exit(1);
+                }
+            }
+        } else {    // protein
+            const string aa_list = "ARNDCQEGHIKLMFPSTWYVXBZX";
+            for (char c: seq1) {
+                if (aa_list.find(c) == string::npos) {
+                    cerr << "Error: Invalid character in Sequence 1, only general amino acid codon are allowed." << endl;
+                }
+            }
+            for (char c: seq2) {
+                if (aa_list.find(c) == string::npos) {
+                    cerr << "Error: Invalid character in Sequence 1, only general amino acid codon are allowed." << endl;
+                }
+            }
+        }
+
+        // Section: Select different score functions for DNA/RNA or protein
+        int (*selected_score)(char, char);  // Using function pointer to select different score function for DNA/RNA or protein alignment
+        // Section: change scores setting
+        if (choice == "DNA" || choice == "RNA") {
+            selected_score = base_score;
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');    // Clear '\n' from previous question
+            char change;
+            cout << "Do you want to change default score setting (Y or N)? (Match: " << Match << ", Mismatch: "
+                 << Mismatch
+                 << ", Gap: " << Gap << endl;
+            cin >> change;
+            if (change != 'Y' && change != 'N') {
+                cerr << "Error: Only Y or N is allowed." << endl;
+                exit(1);
+            } else if (change == 'Y') {
+                cout << "Please enter the new Match, Mismatch, Gap score:" << endl;
+                cin >> Match >> Mismatch >> Gap;
+                if (cin.fail()) {
+                    cerr << "Error: Only integer scores are allowed." << endl;
+                    exit(1);
+                }
+            }
+        } else {
+            BLOSUM_matrix();
+            selected_score = aa_score;
         }
         cin.clear();
         cin.ignore(numeric_limits<streamsize>::max(), '\n');    // Clear '\n' from previous question
@@ -283,13 +364,13 @@ int main(int argc, char* argv[])
         getline(cin, alignment);
         pair<string, string> res;
         if (alignment == "global") {
-            res = global(seq1, seq2);
+            res = global(seq1, seq2, *selected_score);  // input function pointer of selected score function to alignment algorithm
         } else if (alignment == "local") {
-            res = local(seq1, seq2);
+            res = local(seq1, seq2, *selected_score);
         } else if (alignment == "semi-global") {
-            res = semiglobal(seq1, seq2);
+            res = semiglobal(seq1, seq2, *selected_score);
         } else if (alignment == "overlap") {
-            res = overlap(seq1, seq2);
+            res = overlap(seq1, seq2, *selected_score);
         } else {
             cerr << "Error: Invalid input alignment method, only global, local, semi-global or overlap are allowed."
                  << endl;
@@ -297,10 +378,26 @@ int main(int argc, char* argv[])
         }
         cout << res.first << endl;
         cout << res.second << endl;
-        ofstream output("alignment_result_" + alignment + ".txt");
-        output << "Method: " << alignment << endl;
-        output << res.first << endl;
-        output << res.second << endl;
+        std::string output_filename = string(argv[1]) + "_alignment_result_" + alignment + ".txt";
+        std::ofstream output(output_filename);
+        unsigned int SequenceLength = max(res.first.length(), res.second.length());
+        int startpos = 0;       // Divide aligned region into 100bp per line
+        int chunksize = 100;
+        while (startpos < SequenceLength) {
+            string CurrentChunk1 = res.first.substr(startpos, chunksize);
+            string CurrentChunk2 = res.second.substr(startpos, chunksize);
+            string MatchingLine(chunksize, ' ');
+            for (int i = 0; i < min(CurrentChunk1.length(), CurrentChunk2.length()); i++) {
+                if (CurrentChunk1[i] == CurrentChunk2[i]) {
+                    MatchingLine[i] = '|';      // Only add matching line "|" when corresponding character is matched (Never have both gap in pairwise alignment)
+                }
+            }
+            output << CurrentChunk1 << endl;
+            output << MatchingLine << endl;
+            output << CurrentChunk2 << endl;
+            output << endl;
+            startpos += chunksize;
+        }
         output.close();
         return 0;
     }
